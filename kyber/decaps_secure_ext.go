9b92f9b48de3c252593f6982e63fccb05a3aa648
Wujunhui0411 <101546955+Wujunhui0411@users.noreply.github.com>
2025-06-30 00:39:28 +0800

    Update decaps_secure_ext.go

--- kyber/decaps_secure_ext.go
+++ kyber/decaps_secure_ext.go
@@ -1,18 +1,6 @@
 package kyber
 
 import (
-    crystalskyber "github.com/cloudflare/circl/pke/kyber"
-)
-
-// DecapsSecureExt 承接原本的 Secure 解封裝
-func DecapsSecureExt(k crystalskyber.Kyber, sk, c []byte) []byte {
-    // 正常密封後直接使用 crystals-go 解封
-    ss := k.Decaps(sk, c)
-    // 然後再跑一次 Secure 檢查 poly-/reencapsulation
-    return ss 
-package kyber
-
-import (
     "errors"
 
     "github.com/cloudflare/circl/pke/kyber"
@@ -34,4 +22,3 @@ func DecapsSecureExt(scheme *kyber.Scheme, sk []byte, ciphertext []byte) ([]byte
 
     return ss, nil
 }
-
